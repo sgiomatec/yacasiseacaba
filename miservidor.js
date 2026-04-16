6dfea5c221f03f84e9d545723185bb3381b499ae
sgiomatec <63809125+sgiomatec@users.noreply.github.com>
2026-04-16 12:40:33 -0600

    Update miservidor.js

--- miservidor.js
+++ miservidor.js
@@ -1,7 +1,7 @@
 import http from 'http';
 
 const servidor = http.createServer((req, res) => {
-  console.log(“Alguien me mandó una solicitud”);
+  console.log("Alguien me mandó una solicitud");
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Quiero la libertad de esculpir y cincelar mi propio rostro, de detener la hemorragia con cenizas, de crear mis propios dioses a partir de mis entrañas...\n');
 });
